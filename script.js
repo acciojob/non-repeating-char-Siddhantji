0ac022354f6a3facba1ea0df060e6617e94b014a
Siddhant Teotia <91278902+Siddhantji@users.noreply.github.com>
2024-04-05 19:03:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let count = {};
+	for(char of str){
+		count[char]=(count[char] || 0)+1;
+	}
+	for(char of str){
+		if(count[char]===1){
+			return char;
+		}
+	}
+	return null;
 }
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+// const input = prompt("Enter a string");
+// alert(firstNonRepeatedChar(input)); 
